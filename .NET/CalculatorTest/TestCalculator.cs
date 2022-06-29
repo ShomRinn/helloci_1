b799be0f0328b2513bbcfbe4bd27f043a5fadf74
Heorhi <84874114+ShomRinn@users.noreply.github.com>
2022-06-29 21:38:34 +0300

    Update TestCalculator.cs

--- .NET/CalculatorTest/TestCalculator.cs
+++ .NET/CalculatorTest/TestCalculator.cs
@@ -11,38 +11,5 @@ namespace CalculatorTest
         [OneTimeSetUp]
         public void Setup() => Console.WriteLine("Starting TestHelloWorld tests");
 
-        [OneTimeTearDown]
-        public void TearDown() => Console.WriteLine("TestHelloWorld tests are finished");
-
-        [Category("SumTests")]
-        [Test]
-        public void OneCanSumPositiveIntegers() => Assert.That(Calc.Sum(5, 5), Is.EqualTo(10));
-
-        [Category("SumTests")]
-        [Test]
-        public void OneCanSumNegativeIntegers() => Assert.That(Calc.Sum(-5, -5), Is.EqualTo(-10));
-
-        [Category("SubstractTests")]
-        [Test]
-        public void OneCanSubstractPositiveIntegers() => Assert.That(Calc.Substract(5, 5), Is.EqualTo(0));
-
-        [Category("SubstractTests")]
-        [Test]
-        public void OneCanSubstractNegativeIntegers() => Assert.That(Calc.Substract(-5, -5), Is.EqualTo(0));
-
-        [Category("SubstractTests")]
-        [Test]
-        public void OneCanSubstracPositiveIntegertFromZero() => Assert.That(Calc.Substract(0, 100), Is.EqualTo(-100));
-
-        public void OneCanReadAppConfig()
-        {
-            var config = new ConfigurationBuilder()
-                .AddJsonFile("appconfig.json")
-                .Build()
-                .Get<Config>();
-
-            Console.WriteLine($"Browser: {config.BrowserName}");
-            Console.WriteLine($"Timeout: {config.TimeoutSeconds}");
-        }
     }
 }
